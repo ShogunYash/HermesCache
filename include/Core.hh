a2e5bc198e8c59ecf14e4d2cbf19877e26f007f9
Yash Sunil Shindekar <shindekaryash16@gmail.com>
2025-04-27 12:05:13 +0530

    read & write count added

--- include/Core.hh
+++ include/Core.hh
@@ -19,13 +19,15 @@ struct Request {
 class Core {
 public:
     int id;                     // Core identifier (0 to 3)
-    Cache* cache;               // Pointer to the core’s L1 cache
+    Cache* cache;               // Pointer to the core's L1 cache
     std::vector<Request> trace; // Trace of memory requests for the core
     size_t instPtr;             // Instruction pointer in the trace
     uint64_t nextFreeCycle;     // Cycle count when the core becomes unblocked
+    uint64_t readCount;         // Total read operations
+    uint64_t writeCount;        // Total write operations
 
     Core(int id, Cache* cache);
-    // Loads a trace file into the core’s trace vector.
+    // Loads a trace file into the core's trace vector.
     void loadTrace(const std::string& filename);
 };
 
